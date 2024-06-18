9a680330bed3e5d102b55df85f5595ca9f5c1d17
sajan25805 <sajanrajmainali99@gmail.com>
2024-06-19 03:16:26 +0545

    background Color Changer Done

--- simple-chrome-extension/src/App.tsx
+++ simple-chrome-extension/src/App.tsx
@@ -1,26 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [color, setColor] = useState("")
+  const onClick = async () => {
+    let [tab]: chrome.tabs.Tab[] = await chrome.tabs.query({ active: true });
+    if (tab.id !== undefined) {
+      chrome.scripting.executeScript<string[], void>({
+        target: { tabId: tab.id },
+        args: [color],
+        func: (color) => {
+          document.body.style.backgroundColor = color;
+        },
+      });
+    }
+  }
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://vitejs.dev" target="_blank" rel="noopener noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev" target="_blank" rel="noopener noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <label htmlFor=""></label>
+        <input id='color' className='color' type="text" onChange={(e) => setColor(e.currentTarget.value)} />
+        <button onClick={() => onClick()}>Change Color</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
@@ -29,7 +41,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
